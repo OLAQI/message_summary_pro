12cb3f335bd21b969fc5ced9f136baaccdede6ca
OLAQI <137159097+OLAQI@users.noreply.github.com>
2025-02-10 20:09:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import requests
 
 logger = logging.getLogger("astrbot")
 
-@register("Message_Summary", "OLAQI", "群聊消息总结插件", "1.0.0", "https://github.com/OLAQI/message_summary_pro")
+@register("Message_Summary", "OLAQI", "群聊消息总结插件", "1.0.1", "https://github.com/OLAQI/message_summary_pro")
 class GroupSummaryPlugin(Star):
     def __init__(self, context: Context, config: dict):
         super().__init__(context)
